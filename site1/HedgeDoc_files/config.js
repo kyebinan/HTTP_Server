3e4b587e1d9ebca7a71d7cb1b60ac6581dfb65ca
yohan <yohanbinan22@gmail.com>
2024-03-11 13:51:15 +0100

    I've added the site 1

---
+++ site1/HedgeDoc_files/config.js
@@ -0,0 +1,12 @@
+window.domain = '192.168.132.15'
+window.urlpath = ''
+window.debug = false
+window.version = '1.8.2'
+
+window.allowedUploadMimeTypes = ["image/jpeg","image/png","image/gif","image/svg+xml"]
+
+window.linkifyHeaderStyle = 'gfm'
+
+window.DROPBOX_APP_KEY = ''
+
+window.cookiePolicy = 'lax'
